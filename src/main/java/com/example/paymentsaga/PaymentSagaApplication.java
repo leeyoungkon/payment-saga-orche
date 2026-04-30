9098f6943f31f7094cf7300399cb01e486786a9f
leeyoungkon <yklee2002@gmail.com>
2026-04-30 14:54:59 +0900

    first commit

---
+++ src/main/java/com/example/paymentsaga/PaymentSagaApplication.java
@@ -0,0 +1,13 @@
+package com.example.paymentsaga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentSagaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentSagaApplication.class, args);
+	}
+
+}
